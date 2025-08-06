b3527bedef5e91572789faff78b149385ef82bb7
Shruthi KV <shruthikv@Mac.lan>
2025-08-06 22:04:58 +0530

    double loop

---
+++ doubleLoops.js
@@ -0,0 +1,24 @@
+
+// for (let i=0; i< 3 ; i++){
+//     for(let j=0; j<3;j++){
+//         console.log("i = " + i + "" + " j = " + j)
+//     }
+// }
+
+for (let i=0; i< 3 ; i++){
+    for(let j=0; j<i;j++){
+        console.log("i = " + i + "" + " j = " + j)
+    }
+}
+
+// //          * * * * *
+// //          * * * * *
+// //          * * * * *
+// //          * * * * *
+// //          * * * * *
+
+// for(let i = 0 ; i <5 ;i ++){
+//     for(let j=0; j<5;j++){
+//         console.log("* ")
+//     }
+// }
