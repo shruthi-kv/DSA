93734c3ad01e6f48f619f2dde1307d144c36d64a
Shruthi KV <shruthikv@Shruthis-MacBook-Air.local>
2025-10-25 17:15:35 +0530

    dsa

---
+++ sat/bits.js
@@ -0,0 +1,21 @@
+let arr = [0,1,1,1,0,0,0,0,1,1,1,1,0];
+
+
+function countBits(arr){
+    let count = 0;
+    let maxCount = 0;
+
+
+    for(let i=1;i<arr.length;i++){
+        if(arr[i] === arr[i-1]){
+            count++;
+        }else{
+            maxCount = Math.max(count,maxCount)
+            count = 1;
+        }
+    }
+
+    return maxCount;
+}
+
+console.log(countBits(arr))
