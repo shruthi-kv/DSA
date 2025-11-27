f16b1e92ce698a50be65ecc9da3e57acefc39d92
Shruthi KV <shruthikv@Shruthis-MacBook-Air.local>
2025-11-27 08:42:28 +0530

    reverse String2

---
+++ TryAgain/Two.js
@@ -0,0 +1,22 @@
+// let str = "abcdefg";
+let str = "a";
+
+function reverseStringTwo(str) {
+    let a = str.split('')
+
+  for (let i = 0; i < a.length; i = i+4) {
+    if (a[i + 1]) {
+      let temp = a[i];
+      a[i] = a[i + 1];
+      a[i + 1] = temp;
+
+    } else {
+      break;
+    }
+
+  }
+
+  return a.join('');
+}
+
+console.log(reverseStringTwo(str));
