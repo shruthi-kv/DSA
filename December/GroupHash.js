4016e6289661a0be929ca4d228e36e93ef6de72e
Shruthi KV <shruthikv@Shruthis-MacBook-Air.local>
2025-12-14 18:47:39 +0530

    group anagram hash

---
+++ December/GroupHash.js
@@ -0,0 +1,29 @@
+let strs = ["eat","tea","tan","ate","nat","bat"];
+
+function GroupAnagram(strs){
+    let map = {};
+
+    for(let i=0;i<strs.length;i++){
+        
+        let freqArr = Array(26).fill(0);
+        let s = strs[i];
+        for(let j=0;j<s.length;j++){
+            let index = s[j].charCodeAt() - 'a'.charCodeAt();
+            freqArr[index] = freqArr[index]+1;
+        }
+        let key='';
+        for(let k=0;k<26;k++){
+            key = key+"#"+freqArr[k]
+        }
+
+
+
+        if(!map[key]){
+            map[key] = [s]
+        }else{
+            map[key].push(s)
+        }
+    }
+    return [...Object.values(map)]
+}
+console.log(GroupAnagram(strs))
