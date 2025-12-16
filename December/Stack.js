6183b9aeb3f77364512093ff5da2f2e0e8a5f850
Shruthi KV <shruthikv@Shruthis-MacBook-Air.local>
2025-12-16 20:26:15 +0530

    stack and queue

---
+++ December/Stack.js
@@ -0,0 +1,14 @@
+let stack =[];
+stack.push(2)
+stack.push(3)
+stack.push(7)
+stack.push(9)
+
+console.log(stack)
+
+stack.pop()
+
+console.log(stack)
+
+console.log(stack[stack.length-1])
+console.log(stack[1])
