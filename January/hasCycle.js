fd62cb10b8f8a7446b8fb0cac7f434bc6396cf2b
Shruthi KV <shruthikv@Shruthis-MacBook-Air.local>
2026-01-26 19:02:37 +0530

    hascycle

---
+++ January/hasCycle.js
@@ -0,0 +1,16 @@
+var hasCycle = function(head) {
+    if(head == null) return false;
+
+    let slow = head;
+    let fast = head.next;
+
+    while(slow != fast){
+        if(fast == null || fast.next == null){
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return true
+};
